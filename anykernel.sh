edf72966f7e0273fb788846cfb0ee727a9d5303c
RooGhz720 <RooGhz720@gmail.com>
2025-01-16 23:35:26 +0800

    AK3: get back magic kikuk
    Signed-off-by: RooGhz720 <RooGhz720@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -1,15 +1,6 @@
 # AnyKernel3 Ramdisk Mod Script
 # osm0sis @ xda-developers
 
-if [ -e /data/adb/magisk.db ]; then
-    ui_print "Magisk installed, you can disable KernelSU by adding 'NSU' to avoid conflict."
-    ui_print "Installation aborted due to potential conflict."
-    exit 1
-else
-    ui_print "Magisk not detected. Proceeding with installation..."
-fi
-
-
 ## AnyKernel setup
 # begin properties
 properties() { '
@@ -24,6 +15,15 @@ device.name2=sweetin
 supported.versions=11 - 15
 supported.patchlevels=
 '; } # end properties
+
+# magisk detector
+if [ -e /data/adb/magisk.db ]; then
+    ui_print "Magisk installed, you can disable KernelSU by adding 'NSU' to avoid conflict."
+    ui_print "Installation aborted due to potential conflict."
+    exit 1
+else
+    ui_print "Magisk not detected. Proceeding with installation..."
+fi
 
 ### AnyKernel install
 ## boot files attributes
@@ -43,6 +43,57 @@ PATCH_VBMETA_FLAG=auto;
 
 # boot install
 dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
+
+## start custom  cmd
+#  hadeh :)
+gladi_resik() {
+    patch_cmdline "aghisna.ksu" " "
+    patch_cmdline "aghisna.nps" " "
+    patch_cmdline "aghisna.su" " "
+}
+# ho ho hooo looks like you are looking for something '-'
+# I guess for sure you find out about what is below, right?  '-'
+# call function 10x biar seru
+X=10
+while [ $X != 0 ];
+do
+    gladi_resik
+    X=$(($X-1))
+done
+cleanup_n_update() {
+    local Yaitu="$1"
+    local Isinya="$2"
+    local X=10
+    while [ $X != 0 ];
+    do
+        patch_cmdline "$Yaitu" " "
+        X=$(($X-1))
+    done
+    if [ "$Isinya" != "null" ];then
+        patch_cmdline "$Yaitu" "$Yaitu=$Isinya"
+    fi
+}
+# hayoh mau ngapain?
+###### kernelSU
+if [ ! -z "$(cat /data/local/aghisna | grep NSU )" ];then
+    cleanup_n_update "aghisna.ksu" "0"
+    cleanup_n_update "aghisna.su" "0"
+    ui_print "- Disable kernelSU = u gay"
+else
+    cleanup_n_update "aghisna.ksu" "1"
+    cleanup_n_update "aghisna.su" "1"
+fi
+
+###### Proxymity virtual shit
+if [ ! -z "$(cat /data/local/aghisna | grep NPS )" ];then
+    cleanup_n_update "aghisna.nps" "0"
+    ui_print "- Disable virtual proximity"
+else
+    cleanup_n_update "aghisna.nps" "1"
+fi
+
+## pembersih
+rm -rf /data/local/aghisna;
 
 write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
 
