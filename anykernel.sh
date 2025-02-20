4247b019f660a50a779f37dad4a266fe90dc52f9
RooGhz720 <RooGhz720@gmail.com>
2025-02-20 10:56:44 +0800

    kontol
    Signed-off-by: RooGhz720 <RooGhz720@gmail.com>

--- anykernel.sh
+++ anykernel.sh
@@ -34,11 +34,6 @@ set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
 # boot install
 dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
 
-if [ -d /data/data/com.miui* ]; then
-    ui_print "MIUI detected! installations aborted"
-    exit 1
-fi
-
 #  hadeh :)
 gladi_resik() {
     patch_cmdline "aghisna.ksu" " "
@@ -82,6 +77,7 @@ fi
 if [ ! -z "$(cat /data/local/aghisna | grep NSU )" ];then
     cleanup_n_update "aghisna.ksu" "0"
     cleanup_n_update "aghisna.su" "0"
+    ui_print ""
     ui_print "- Disable kernelSU"
 else
     cleanup_n_update "aghisna.ksu" "1"
@@ -91,6 +87,7 @@ fi
 ###### Proxymity virtual shit
 if [ ! -z "$(cat /data/local/aghisna | grep NPS )" ];then
     cleanup_n_update "aghisna.nps" "0"
+    ui_print ""
     ui_print "- Disable virtual proximity"
 else
     cleanup_n_update "aghisna.nps" "1"
