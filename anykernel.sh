68e27a94363d98d454fd15af29816ba77a96194c
RooGhz720 <RooGhz720@gmail.com>
2025-02-26 05:00:27 +0800

    bfyfgu

--- anykernel.sh
+++ anykernel.sh
@@ -11,7 +11,7 @@ do.cleanup=1
 do.cleanuponabort=0
 device.name1=sweet
 device.name2=sweetin
-supported.versions=11 - 13
+supported.versions=11 - 15
 supported.patchlevels=
 '; } # end properties
 
@@ -33,66 +33,6 @@ set_perm_recursive 0 0 750 750 $RAMDISK/init* $RAMDISK/sbin;
 
 # boot install
 dump_boot; # use split_boot to skip ramdisk unpack, e.g. for devices with init_boot ramdisk
-
-# magisk detector
-if [ -e /data/adb/magisk.db ]; then
-    ui_print "Magisk installed, you can disable KernelSU by adding 'NSU' to avoid conflict!"
-    ui_print "backup your modules, use stock kernel without magisk patch then delete adb folder"
-    ui_print "/data/adb < delete this"
-    ui_print "instalation terminated!"
-    exit 1
-fi
-
-#  hadeh :)
-gladi_resik() {
-    patch_cmdline "aghisna.ksu" " "
-    patch_cmdline "aghisna.nps" " "
-    patch_cmdline "aghisna.su" " "
-}
-# ho ho hooo looks like you are looking for something '-'
-# I guess for sure you find out about what is below, right?  '-'
-# call function 10x biar seru
-X=10
-while [ $X != 0 ];
-do
-    gladi_resik
-    X=$(($X-1))
-done
-cleanup_n_update() {
-    local Yaitu="$1"
-    local Isinya="$2"
-    local X=10
-    while [ $X != 0 ];
-    do
-        patch_cmdline "$Yaitu" " "
-        X=$(($X-1))
-    done
-    if [ "$Isinya" != "null" ];then
-        patch_cmdline "$Yaitu" "$Yaitu=$Isinya"
-    fi
-}
-
-# hayoh mau ngapain?
-###### kernelSU
-if [ ! -z "$(cat /data/local/aghisna | grep NSU )" ];then
-    cleanup_n_update "aghisna.ksu" "0"
-    cleanup_n_update "aghisna.su" "0"
-    ui_print "- Disable kernelSU"
-else
-    cleanup_n_update "aghisna.ksu" "1"
-    cleanup_n_update "aghisna.su" "1"
-fi
-
-###### Proxymity virtual shit
-if [ ! -z "$(cat /data/local/aghisna | grep NPS )" ];then
-    cleanup_n_update "aghisna.nps" "0"
-    ui_print "- Disable virtual proximity"
-else
-    cleanup_n_update "aghisna.nps" "1"
-fi
-
-## pembersih
-rm -rf /data/local/aghisna;
 
 write_boot; # use flash_boot to skip ramdisk repack, e.g. for devices with init_boot ramdisk
 ui_print "=========================="
